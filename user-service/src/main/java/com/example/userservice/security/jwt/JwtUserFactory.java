8a0ec6fac87f2dcb40d4d0a6b41e19cdc7d506a8
Maria Zakharevich <zaxarevich_ma@yahoo.com>
2023-03-19 21:46:02 +0300

    product-service

--- user-service/src/main/java/com/example/userservice/security/jwt/JwtUserFactory.java
+++ user-service/src/main/java/com/example/userservice/security/jwt/JwtUserFactory.java
@@ -1,15 +1,11 @@
 package com.example.userservice.security.jwt;
 
-import com.example.userservice.dao.entity.RoleEntity;
 import com.example.userservice.dao.entity.UserEntity;
-import com.example.userservice.dto.EUserRole;
-import com.example.userservice.dto.UserRole;
 import com.example.userservice.dto.UserStatus;
 import com.example.userservice.utils.convertors.RoleConverter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -27,16 +23,17 @@ public final class JwtUserFactory {
                 user.getPassword(),
                 user.getStatus().equals(UserStatus.ACTIVATED),
                 user.getDt_update(),
-                mapToGrantedAuthorities(new ArrayList<>((Collection) converter.convert(user)))
+                null
+ //               mapToGrantedAuthorities(new ArrayList<>((Collection) converter.convert(user)))
         );
     }
 
-    private static List<GrantedAuthority> mapToGrantedAuthorities(List<RoleEntity> userRoles){
+/*    private static List<GrantedAuthority> mapToGrantedAuthorities(List<RoleEntity> userRoles){
         return userRoles.stream()
                 .map(role ->
                      new SimpleGrantedAuthority(role.getName())
                 )
                 .collect(Collectors.toList());
-    }
+    }*/
 
 }
